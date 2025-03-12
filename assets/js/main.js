c57545fac7f0c8c3ee598c126b96a81d09e8f6db
Alex <Alex.007.Writer@gmail.com>
2025-03-12 19:02:08 +0200

    2

--- assets/js/main.js
+++ assets/js/main.js
@@ -168,15 +168,7 @@
   document.addEventListener('scroll', navmenuScrollspy);
 
 
-  function openPopup() {
-    document.getElementById("popup").style.display = "block";
-    document.getElementById("popupOverlay").style.display = "block";
-  }
 
-  function closePopup() {
-    document.getElementById("popup").style.display = "none";
-    document.getElementById("popupOverlay").style.display = "none";
-  }
 })();
 
 
